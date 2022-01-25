7ba0e06138b1d6868855b5eb622336299f5c1f27
sgcsfslr <93635619+sgcsfslr@users.noreply.github.com>
2022-01-25 14:03:30 +0800

    Update PM25.py
    Improved version

--- PM25.py
+++ PM25.py
@@ -1,7 +1,7 @@
 # Air quality Indicator app
 # pm25.py
 # Reference from China University Module
-
+""" Old version
 def main(): # define function call "main"
     PM = eval(input("What is today's PM2.5? ")) # Prompt for an input value
     if PM > 75:
@@ -10,5 +10,17 @@ def main(): # define function call "main"
         print("Moderate. Wear masks")
     if PM < 35:
         print("Good. Go running!")
+
+main()
+""""
+# Improved version
+def main(): # define function call "main"
+    PM = eval(input("What is today's PM2.5? ")) # Prompt for an input value
+    if PM < 35:
+        print("Good. Go running!")
+    elif PM <= 75:
+        print("Moderate. Wear masks!")
+    elif PM > 75:
+        print("Unhealty. Be careful!")
 
 main()

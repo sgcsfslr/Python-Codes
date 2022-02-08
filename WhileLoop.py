9c1cd0075e5e135f47d4be7160c89dd45aad475c
sgcsfslr <93635619+sgcsfslr@users.noreply.github.com>
2022-02-08 13:28:01 +0800

    Add files via upload
    The Basic understanding of While Loop function and how to create a simple while loop print program.

---
+++ WhileLoop.py
@@ -0,0 +1,15 @@
+# While loop
+cities = ['New York', 'Washington', 'Los Angles', 'San Francisco', 'Chicago', 'Florida', 'Texas']
+#print(cities)
+number = len(cities)
+#print(number)
+number = number - 1
+#print(number)
+round = 0
+#print(round)
+while round <= number:
+ print(cities[round])
+ #print(round)
+ round = round + 1
+
+print('End of the loop')
